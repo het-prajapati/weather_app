37ecf3bfbe0f60555e9936c0dd4fcce7e72cc510
hetansh9 <hetansh.patel@icloud.com>
2021-12-20 11:34:37 -0700

    updated and added in package223232

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component}from 'react';
-import {Button, Modal} from '@mui/material/Button';
+import {Card} from 'react-bootstrap';
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -8,18 +8,18 @@ class App extends Component {
   render(){
     return (
       <Card style={{ width: '18rem' }}>
+      
         <Card.Body>
           <Card.Title>Card Title</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the bulk of
             the card's content.
           </Card.Text>
+          
         </Card.Body>
       </Card>
     );
   }
 }
-
-
 
 export default App;
