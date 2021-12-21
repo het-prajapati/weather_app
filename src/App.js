3de4b4b8aed39f7f410807122aec5590c7487ec7
Het Prajapati <hprajapati@albany.edu>
2021-12-21 16:34:48 -0500

    Card component Rendering
    Added "homepage": ".", and react-router-dom dependency

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, {Component}from 'react';
-import {Card} from 'react-bootstrap';
+import React, { Component } from "react";
+import { Card } from "react-bootstrap";
 import logo from "./logo.svg";
 import "./App.css";
 
-
 class App extends Component {
-  render(){
+  render() {
     return (
-      <Card style={{ width: '18rem' }}>
-      
+      <Card style={{ width: "18rem" }}>
         <Card.Body>
           <Card.Title>Card Title</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
           </Card.Text>
-          
         </Card.Body>
       </Card>
     );
